2d185d688b4d0d9d535ae2c79d6cf627298a8ea5
dr1nky <leedrinkwater@hotmail.com>
2020-02-27 10:45:10 +0000

    init

---
+++ main.js
@@ -0,0 +1,27 @@
+const menuToggle = document.querySelector('.menu-toggle')
+const mainNav = document.querySelector('.main-nav')
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('close');
+  mainNav.classList.toggle('show');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sliders
+new Glide('.glide').mount()
+new Glide('.glide-2', {
+  gap: 6,
+  autoplay: 10000
+}).mount()
